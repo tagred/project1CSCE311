9d29bb56476bd51b1e709e57cf8451f296dfe8e2
MASSELLI <masselli@cocsce-l1d43-36.ds.sc.edu>
2018-09-23 16:28:16 -0400

    v2

--- main.cc
+++ main.cc
@@ -3,8 +3,28 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void readpipe (string file) {
+void readpipe (int file) {
 FILE *stream;
+std::string c;
+stream =fdopen (file, "r");
+while ((c = fgets (stream)) != EOf) {
+if (c.find(myarg)) {
+std::cout << c << std::endl;
+} 
+}
+
+fclose (stream);
+}
+
+void writepipe (int file) {
+FILE *stream;
+stream = fdopen (file, "w");
+std::ofstream myfile (filepath);
+while (getline (myfile ,line)){
+stream << line << \n;
+}
+fclose (stream);
+myfile.close();
 
 }
 
@@ -14,6 +34,8 @@ int main (int argc, char *argv[]) {
 if (argc > 1) { // check to see for valid amount of arguments
 
 }
+std::string myfile =argv[1];
+std::string myarg = argv[2];
 pid_t pid;
 int mypipe[2];
 
@@ -30,19 +52,18 @@ if (pipe (mypipe)){ //fails if true pipe failed
 
 pid = fork();
 
-if (pid == (pid_t) 0) {
+if (pid == (pid_t) 0) { //child
 close (mypipe[1]);
-{
-close (mypipe[1]);
-read_frim_pipe (mypipe[0]);
+readpipe (mypipe[0]);
+//do something with the writing
 }
 else if ( pid < (pid_t) 0)
 {
 //failed
 }
-else {
+else {   // parent
 close (mypipe[0]);
-write_to_pipe ( mypipe[1]);
+writepipe ( mypipe[1]);
 
 }
 
