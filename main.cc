026798d090a652ea06b100c82b4d87c29e364ab6
MASSELLI <masselli@cocsce-l1d43-36.ds.sc.edu>
2018-09-23 16:46:01 -0400

    cpplint fixes

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void readpipe (int file) {
+void readpipe(int file) {
 FILE *stream;
 std::string c;
 stream =fdopen (file, "r");
@@ -13,65 +13,45 @@ std::cout << c << std::endl;
 } 
 }
 
-fclose (stream);
+fclose(stream);
 }
 
-void writepipe (int file) {
+void writepipe(int file) {
 FILE *stream;
-stream = fdopen (file, "w");
-std::ofstream myfile (filepath);
-while (getline (myfile ,line)){
+stream = fdopen(file, "w");
+std::ofstream myfile(filepath);
+while (getline(myfile ,line) != EOF) {
 stream << line << \n;
 }
-fclose (stream);
+fclose(stream);
 myfile.close();
-
 }
 
 
 
-int main (int argc, char *argv[]) {
-if (argc > 1) { // check to see for valid amount of arguments
-
+int main(int argc, char *argv[]) {
+if (argc == 1) {  // check to see for valid amount of arguments
 }
+
 std::string myfile =argv[1];
 std::string myarg = argv[2];
 pid_t pid;
 int mypipe[2];
 
 
-
-if (pipe (mypipe)){ //fails if true pipe failed
-
-} 
-
-
-
-
-
+if (pipe(mypipe)) {  // fails if true pipe failed
+}
 
 pid = fork();
 
-if (pid == (pid_t) 0) { //child
-close (mypipe[1]);
-readpipe (mypipe[0]);
-//do something with the writing
+if (pid == (pid_t) 0) {  // child
+close(mypipe[1]);
+readpipe(mypipe[0]);
+// do something with the writing
+} else if (pid < (pid_t) 0) {
+// failed
+} else {   // parent
+close(mypipe[0]);
+writepipe(mypipe[1]);
 }
-else if ( pid < (pid_t) 0)
-{
-//failed
-}
-else {   // parent
-close (mypipe[0]);
-writepipe ( mypipe[1]);
-
-}
-
-
-
-
-
-
-
-
 }
