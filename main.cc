9c1841dd7aa515c455463c01568495c4412a5f37
MASSELLI <masselli@cocsce-l1d39-11.ds.sc.edu>
2018-10-13 20:27:38 -0400

    everything compiles

--- main.cc
+++ main.cc
@@ -2,16 +2,19 @@
 #include <fstream>
 #include <unistd.h>
 #include <stdio.h>
+#include <vector>
+#include <sstream>
 
-void readpipe(int file) {
+void readpipe(int file, std::string myarg) {
 FILE *stream;
-std::string *c;
 stream =fdopen (file, "r");
-
-//while ((c = gets(stream)) != EOF) {
-//if (c.find(myarg)) {
-//std::cout << c << std::endl;
-//} 
+std::vector<std::string> array;
+std::stringstream ss;
+char temp[200];
+while(!feof(stream)) {
+fgets(temp, 200, stream);
+std::cout<< temp << std::endl;
+array.push_back(temp);
 }
 
 fclose(stream);
@@ -20,20 +23,31 @@ fclose(stream);
 void writepipe(int file, std::string filepath) {
 FILE *stream;
 stream = fdopen(file, "w");
+<<<<<<< Updated upstream
 std::ofstream myfile(filepath);
 std::string line;
 while (getline(myfile, line) != EOF) {
 stream << line<< std::endl;
 //std::fprintf (stream, line);
+=======
+std::ifstream myfile(filepath);
+std::string temp;
+while(std::getline(myfile, temp)) {
+std::cout<< temp << std::endl;
+fputs(temp.c_str(), stream);
+>>>>>>> Stashed changes
 }
-fclose(stream);
 myfile.close();
+fclose(stream);
+
 }
 
 
 
 int main(int argc, char *argv[]) {
 if (argc == 1) {  // check to see for valid amount of arguments
+std::cout << "invalid args" << std::endl;
+return 0;
 }
 
 std::string myfile =argv[1];
@@ -55,6 +69,6 @@ readpipe(mypipe[0], myarg);
 // failed
 } else {   // parent
 close(mypipe[0]);
-writepipe(mypipe[1], filepath);
+writepipe(mypipe[1], myfile);
 }
 }
