fc6d5a034a0a9efdaf1af60dda1ed65caf0b741b
MASSELLI <masselli@cocsce-l1d39-11.ds.sc.edu>
2018-10-14 17:42:50 -0400

    added comments to main.cc

--- main.cc
+++ main.cc
@@ -10,27 +10,28 @@
 
 
 
-void readpipe(int file, std::string myarg) {
+void readpipe(int file, std::string myarg) { // child process reads pipe and gives output
 FILE *stream;
-stream =fdopen (file, "r");
+stream =fdopen (file, "r"); // open to read
 std::vector<std::string> array;
-char temp[2000];
-while(!feof(stream)) {
-fgets(temp, 2000, stream);
-if (std::strlen(temp) != 2)
-array.push_back(temp);
+char temp[1024];
+while(!feof(stream)) { // loops until the stream is empty
+fgets(temp, 1024, stream); // pulls off a line from the stream
+array.push_back(temp); // places it on the back of a vector of strings
 }
-fclose(stream);
- 
+fclose(stream); // closes the pipe since we have gotten all the info from it
+
+//builds regex
 std::string myregex = "[^A-z](";
 myregex.append(myarg);
 myregex.append(")[^A-z]");
 std::regex e (myregex, std::regex_constants::icase);
 
+//sorts
 std::sort(array.begin(), array.end());
 
-for(std::vector<std::string>::iterator it = array.begin(); it != array.end(); ++it) {
-    if(std::regex_search(*it,e)) {
+for(std::vector<std::string>::iterator it = array.begin(); it != array.end(); ++it) { //iterates vector
+    if(std::regex_search(*it,e)) { // checks to see if that lines contains the word we are looking for if so print the line
 std::cout << *it;
 }
 }
@@ -38,14 +39,14 @@ std::cout << *it;
 
 void writepipe(int file, std::string filepath) {
 FILE *stream;
-stream = fdopen(file, "w");
-std::ifstream myfile(filepath);
+stream = fdopen(file, "w");  //open to write
+std::ifstream myfile(filepath); // create the stream of the file
 std::string temp;
-while(std::getline(myfile, temp)) {
+while(std::getline(myfile, temp)) { // loop until there are no more lines in the file
 //std::cout << temp << std::endl;
-if(temp.compare("") != 0){
-temp.append("\n");
-fputs(temp.c_str(), stream);
+if(temp.compare("") != 0){ // checks to see if its empty
+temp.append("\n"); // adds a new line
+fputs(temp.c_str(), stream); // puts the new line on the stream
 }
 temp = "";
 }
@@ -66,6 +67,7 @@ return 0;
 }
 std::string myfile =argv[1];
 std::string myarg = argv[2];
+
 pid_t pid;
 int mypipe[2];
 
@@ -73,7 +75,7 @@ int mypipe[2];
 if (pipe(mypipe)) {  // fails if true pipe failed
 }
 
-pid = fork();
+pid = fork(); // gets the pid 0 if child and if parent gets pid of child
 
 if (pid == (pid_t) 0) {  // child
 close(mypipe[1]);
